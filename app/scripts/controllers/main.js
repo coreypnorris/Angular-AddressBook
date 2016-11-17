d68f809e91c0b83863a99033d4b43411f7198b78
Corey Norris <corey.norris@bluevolt.com>
2016-11-17 11:22:08 -0800

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name addressBookApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the addressBookApp
+ */
+angular.module('addressBookApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
