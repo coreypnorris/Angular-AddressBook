7ec9e6a478168f96d17757540a16abdb0b893882
Corey Norris <corey.norris@bluevolt.com>
2016-11-18 13:52:43 -0800

    Refactor contact properties

--- test/spec/services/contacts.js
+++ test/spec/services/contacts.js
@@ -18,15 +18,19 @@ describe('Service: contacts', function () {
 
   it('should have return contacts with certain properties', function () {
     var data = contacts.data();
-    for (var i = 0; i < data.length; i++) {
-      var contact = data[i];
-      expect(contact.id >= 1).toBe(true);
-      expect(contact.name.length > 1).toBe(true);
-      expect(contact.address1.length > 1).toBe(true);
-      expect(contact.address2.length > 1).toBe(true);
-      expect(contact.phone.length > 1).toBe(true);
-      expect(contact.image.length > 1).toBe(true);  
-      expect(typeof(contact.favorite) === "boolean").toBe(true);
-    }
+    var firstContact = data[0];
+    expect(firstContact.id === 1).toBe(true);
+    expect(firstContact.firstname === "Kaylyn").toBe(true);
+    expect(firstContact.lastname === "Gentry").toBe(true);
+    expect(firstContact.address1 === "47 2nd Lane").toBe(true);
+    expect(firstContact.address2 === "").toBe(true);
+    expect(firstContact.city === "Mountain View").toBe(true);
+    expect(firstContact.state === "CA").toBe(true);
+    expect(firstContact.postalcode === "94043").toBe(true);
+    expect(firstContact.phone === "(405) 718-6173").toBe(true);
+    expect(firstContact.email === "Treff1940@gustr.com").toBe(true);
+    expect(firstContact.dateofbirth === "9/26/1976").toBe(true);
+    expect(firstContact.image === "https://images.unsplash.com/photo-1415471659643-57043b686f87?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=2576adcec53bb1ee41558ee540ac44b9").toBe(true);
+    expect(firstContact.favorite === true).toBe(true);
   });
 });
